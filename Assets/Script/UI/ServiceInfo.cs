4a199fe7c5e1b921c8c891b62fc2ff0414256d46
MibuWolf <sanmuwangzi@126.com>
2017-08-24 20:56:30 +0800

    服务器列表

---
+++ Assets/Script/UI/ServiceInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceInfo : MonoBehaviour {
+
+    //  服务器IP端口号
+    public string IP = "";
+
+    //  服务器名称
+    public string name
+    {
+        get { return name; }
+
+        set
+        {   
+            transform.Find("Label").GetComponent<UILabel>().text = value;;
+        }
+    }
+
+    //  服务器人数
+    public int count = 0;
+}
--- Assets/Script/UI/StartGameUI.cs
+++ Assets/Script/UI/StartGameUI.cs
@@ -3,17 +3,31 @@ using System.Collections;
 
 public class StartGameUI : MonoBehaviour {
 
+    //  各个界面
     public TweenScale entergame;
 	public TweenScale login;
 	public TweenScale register;
+    public TweenScale service;
 
+    //  登陆界面角色名称
     public UILabel lb_loginName;
+    //  进入游戏的角色名称
     public UILabel lb_entergameName;
+
+    // 服务器列表
+    public UIGrid servicelist;
+
+    //  火爆服务器预设
+    public GameObject hb_obj;
+    //  流畅服务器预设
+    public GameObject lc_obj;
 
 	// Use this for initialization  
 	void Start () {
-	
-	}
+
+        initServiceList();
+
+    }
 
     // 点击进入游戏选择用户按钮
     public void onClickUserName()
@@ -29,8 +43,12 @@ public class StartGameUI : MonoBehaviour {
 	// 点击服务器更换按钮
 	public void onClickChangeService()
 	{
-		
-	}
+        service.PlayForward();
+        service.gameObject.SetActive(true);
+        entergame.PlayForward();
+
+        StartCoroutine(Wait(entergame.gameObject));
+    }
 
 
 	// 点击开始游戏
@@ -79,10 +97,39 @@ public class StartGameUI : MonoBehaviour {
         
     }
 
+    //  初始化服务器列表
+    private void initServiceList()
+    {
+        for (int i = 1; i < 21; i++)
+        {
+            string ip = "127.0.0.1:8080";
+            string name = i.ToString() + "区 勇者大陆";
+            int count = Random.Range(0, 100);
+
+            GameObject obj = null;
+
+            if (count > 50)
+            {
+                obj = NGUITools.AddChild(servicelist.gameObject, hb_obj);
+            }
+            else
+            {
+                obj = NGUITools.AddChild(servicelist.gameObject, lc_obj);
+            }
+
+            ServiceInfo info = obj.GetComponent<ServiceInfo>();
+            info.IP = ip;
+            info.name = name;
+            info.count = count;
+            servicelist.AddChild(obj.transform);
+        }
+    }
+
 
     IEnumerator Wait( GameObject obj )  
 	{  
 		yield return new WaitForSeconds(0.8f);
         obj.SetActive (false);
-	}  
+	}
+
 }
