8cb90fdff79d7319ea9d29c87d3c5ad04054fa46
MibuWolf <sanmuwangzi@126.com>
2017-11-10 19:47:55 +0800

    挂上固定视角摄像机

---
+++ Assets/Script/Camera/FixedCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour {
+
+    private Vector3 fixedPos = new Vector3(0, 5, -12);
+
+    private Transform mainRole;
+	// Use this for initialization
+	void Awake ()
+    {
+        mainRole = GameObject.FindGameObjectWithTag("Player").transform;	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (mainRole == null)
+            return;
+
+        transform.position = mainRole.position + fixedPos;
+
+
+    }
+}
--- Assets/Script/MainRole/MainRoleMove.cs
+++ Assets/Script/MainRole/MainRoleMove.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MainRoleMove : MonoBehaviour {
 
-    private int speed = 10;
+    private int speed = 8;
 
     private Vector3 helpVec = new Vector3();
 
     private Rigidbody rigBody;
 
+    private Animator animator;
+
+    private Vector3 helpRot = new Vector3();
+
     private void Awake()
     {
         rigBody = this.transform.GetComponent<Rigidbody>();
+        animator = this.transform.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,8 +30,18 @@ public class MainRoleMove : MonoBehaviour {
         helpVec.y = rigBody.velocity.y;
         helpVec.z = v * speed;
 
+        helpRot.x = h;
+        helpRot.y = 0;
+        helpRot.z = v;
 
         rigBody.velocity = helpVec;
 
+        if (rigBody.velocity.magnitude > 0.01f)
+            animator.SetBool("Run", true);
+        else
+            animator.SetBool("Run", false);
+
+        if( Mathf.Abs( h ) > 0.01f || Mathf.Abs( v ) > 0.01f)
+            transform.rotation = Quaternion.LookRotation(helpRot);
     }
 }
